fb881f472e86e475c60a9ba935684965c6c37912
Duy13091995 <151107696+Duy13091995@users.noreply.github.com>
2023-11-20 10:21:54 +0700

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#intro-chay').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('#sanpham-chay').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
